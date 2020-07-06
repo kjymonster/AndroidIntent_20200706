13a7aa711207bcb1f294c3b547c3422d36c093ad
김재영 <kjymonster@naver.com>
2020-07-06 19:48:02 +0900

    Dial 액션 테스트 - 통화 버튼 누르기 직전

--- app/src/main/java/kr/co/tjoeun/androidintent_20200706/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/androidintent_20200706/MainActivity.kt
@@ -1,11 +1,35 @@
 package kr.co.tjoeun.androidintent_20200706
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+            dialBtn.setOnClickListener {
+
+                //입력한 폰번 받아오기
+                val inputPhoneNum = phoneNumEdt.text.toString()
+
+                //어디로 전화를 걸지 정보 저장 변수 => Uri
+                //parse = String을 변환
+                val myUri = Uri.parse("tel:${inputPhoneNum}")
+
+                //실제 전화 화면으로 이동시켜줄 Intent
+                //어떤 행동을 할지 종류, 세부정보
+                val myIntent = Intent(Intent.ACTION_DIAL, myUri)
+
+                //액티비티 실행
+                startActivity(myIntent)
+            }
+
+
+
+
     }
 }
