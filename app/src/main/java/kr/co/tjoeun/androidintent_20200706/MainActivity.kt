2c380f1f0e5f1433324f6d66f38685d9d19f5762
김재영 <kjymonster@naver.com>
2020-07-06 20:42:27 +0900

    네이버 웹링크 /카카오 스토어링크

--- app/src/main/java/kr/co/tjoeun/androidintent_20200706/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/androidintent_20200706/MainActivity.kt
@@ -31,9 +31,7 @@ class MainActivity : AppCompatActivity() {
             callBtn.setOnClickListener {
 
                 val inputPhone = phoneNumEdt.text.toString()
-
                 val myUri = Uri.parse("tel:${inputPhone}")
-
                 val myIntent = Intent(Intent.ACTION_CALL, myUri)
 
 
@@ -43,16 +41,27 @@ class MainActivity : AppCompatActivity() {
             smsBtn.setOnClickListener {
 
                 val inputPhone = phoneNumEdt.text.toString()
-
                 val inputContent = smsContentEdt.text.toString()
-
                 val myUri = Uri.parse("smsto:${inputPhone}")
-
                 val myIntent = Intent(Intent.ACTION_SENDTO, myUri)
 
                 //입력한 내용을 putExtra,로 들고가자  (sms_body라고 정해져 있음)
                 myIntent.putExtra("sms_body", inputContent)
                 startActivity(myIntent)
+            }
+
+            naverWebLinkBtn.setOnClickListener {
+                val myUri = Uri.parse("https://naver.com")
+                val myIntent = Intent(Intent.ACTION_VIEW, myUri)
+                startActivity(myIntent)
+            }
+
+            kakaoPlayStoreBtn.setOnClickListener {
+                val myUri = Uri.parse("market://details?id=com.kakao.talk")
+                val myIntent = Intent(Intent.ACTION_VIEW, myUri)
+                startActivity(myIntent)
+
+
             }
 
 
